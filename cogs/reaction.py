6580906f9addf22739cc5270c9e13a5d9cf29296
maselelele <contact@maselelele.me>
2020-05-11 21:48:39 +0200

    Implement channel creation and deletion

--- cogs/reaction.py
+++ cogs/reaction.py
@@ -20,14 +20,38 @@ class Reaction(commands.Cog):
             reaction_guild = self.client.get_guild(payload.guild_id)
             reaction_channel = self.client.get_channel(payload.channel_id)
             reaction_message = await reaction_channel.fetch_message(payload.message_id)
+            reaction_category = self.client.get_channel(
+                data[f'{payload.guild_id}']['channel_ids'][0])
             await reaction_message.remove_reaction(payload.emoji, payload.member)
 
+            # TODO: Manage channel names using the channel topic, too
             if payload.emoji.name == '\U0001F7E2':
                 print('Create Channel...')
                 # Channel creation here
+                user_channel = await reaction_guild.create_text_channel(name=f'{self.client.get_user(payload.user_id).name}', category=reaction_category)
+                with open('data/data.json', 'r') as f:
+                    data = json.load(f)
+                with open('data/data.json', 'w') as f:
+                    data[f'{reaction_guild.id}']['channel_ids'].append(
+                        user_channel.id)
+                    json.dump(data, f, indent=4)
+
             elif payload.emoji.name == '\U0001F534':
                 print('Delete Channel...')
                 # Channel deletion here
+                with open('data/data.json', 'r') as f:
+                    data = json.load(f)
+                try:
+                    for channel in reaction_guild.channels:
+                        if channel.id in data[f'{reaction_guild.id}']['channel_ids']:
+                            if channel.name == self.client.get_user(payload.user_id).name.lower():
+                                await channel.delete()
+                                with open('data/data.json', 'w') as f:
+                                    data[f'{reaction_guild.id}']['channel_ids'].remove(
+                                        channel.id)
+                                    json.dump(data, f, indent=4)
+                except KeyError as err:
+                    print('Channel not found')
 
 
 def setup(client):
